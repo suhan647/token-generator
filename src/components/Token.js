55ea3d901a42eb6967906133406f990d59ae888b
suhan647 <suhanahmed647@gmail.com>
2023-01-09 23:35:27 +0530

    added generating blue token with prefix functionality

--- src/components/Token.js
+++ src/components/Token.js
@@ -1,47 +1,70 @@
-import React from 'react'
 import '../App.css'
+import React, { useState } from 'react'
 import TokenInputs from './TokenInputs'
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 
 const Token = () => {
-  return (
-    <>
-    <div className='heading'>Token Generator Application</div>
-     <TokenInputs />
-     <Box sx={{marginLeft:"25%"}}>
-     <Box sx={{ marginTop:"40px" }}>
-      <Grid container >
-        <Grid  item xs={2}>
-          <div className='blue-boxes'>xs=2</div>   
-        </Grid>
-        <Grid  item xs={2}>
-          <div className='blue-boxes'>xs=2</div>   
-        </Grid>
-        <Grid  item xs={2}>
-          <div className='blue-boxes'>xs=2</div>   
-        </Grid>
-       
-      </Grid>
-    </Box>
 
-    <Box className='margin' marginBottom='20%'>
-        
-      <Grid container >
-        <Grid className='margin' item xs={2}>
-          <div className='red-boxes'>xs=2</div>   
-        </Grid>
-        <Grid className='margin' item xs={2}>
-          <div className='red-boxes'>xs=2</div>   
-        </Grid>
-        <Grid className='margin' item xs={2}>
-          <div className='red-boxes'>xs=2</div>   
-        </Grid>
-        </Grid>
-    </Box>
-    </Box>
-    </>
-  )
+    const [blueToken, setBlueToken] = useState(0)
+    const [bluePrefix, setBluePrefix] = useState('')
+    const [blueRow, setBlueRow] = useState(0)
+    const [blueArray, setBlueArray] = useState([])
+
+    let bluearr = []
+    const HandleGenerate = () => {
+
+        for (let i = 1; i <= blueToken; i++) {
+            bluearr.push(i)
+            setBlueArray([...bluearr])
+            setBluePrefix(bluePrefix)
+        }
+        console.log([blueArray]);
+    }
+
+
+    const clear = () => {
+        setBlueArray([])
+        console.log(blueArray);
+    }
+
+    return (
+        <>
+            <div className='heading'>Token Generator Application</div>
+            <TokenInputs HandleGenerate={HandleGenerate} blueToken={blueToken} setBlueToken={setBlueToken} bluePrefix={bluePrefix} setBluePrefix={setBluePrefix} blueRow={blueRow} setBlueRow={setBlueRow} clear={clear} />
+
+            <Box sx={{ marginLeft: "25%" }}>
+                <Box sx={{ marginTop: "40px" }}>
+                    <Grid container >
+                        {blueArray.map((token, i) => {
+                            return (
+                                <>
+                                    <Grid key={i} item xs={2}>
+                                        <div className='blue-boxes'>{bluePrefix}{token}</div>
+                                    </Grid>
+                                </>
+                            )
+                        })}
+                    </Grid>
+                </Box>
+
+                <Box className='margin' marginBottom='20%'>
+
+                    <Grid container >
+                        <Grid className='margin' item xs={2}>
+                            <div className='red-boxes'>xs=2</div>
+                        </Grid>
+                        <Grid className='margin' item xs={2}>
+                            <div className='red-boxes'>xs=2</div>
+                        </Grid>
+                        <Grid className='margin' item xs={2}>
+                            <div className='red-boxes'>xs=2</div>
+                        </Grid>
+                    </Grid>
+                </Box>
+            </Box>
+        </>
+    )
 }
 
 export default Token
--- src/components/TokenInputs.js
+++ src/components/TokenInputs.js
@@ -1,44 +1,41 @@
-import React from 'react'
 import '../App.css'
 import TextField from '@mui/material/TextField'
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button'
 
-const TokenInputs = () => {
-  return (
-    <>
-    <Box>
-    <form className='container'>
-        <Box sx={{display:"flex",flexDirection:'column',justifyContent:'center',marginRight:'30%',color:'blue'}}>
-            <Box className='blue-container'>Generate Blue Token</Box>
-    <TextField  id="outlined-basic" label="No. of blue Tokens" variant="outlined" />
-    <br/>
-    <TextField id="outlined-basic" label="Blue token prefix" variant="outlined" />
-    <br/>
-    <TextField id="outlined-basic" label="Blue token per row" variant="outlined" />
-    
-    </Box>
-   
-    <Box sx={{display:"flex",flexDirection:'column',justifyContent:'center',width:'30%'}}>
-    <Box className='red-container'>Generate Red Token</Box>
-    <TextField id="outlined-basic" label="No. of Red Tokens" variant="outlined" />
-    <br/>
-    <TextField id="outlined-basic" label="Red token prefix" variant="outlined" />
-    <br/>
-    <TextField id="outlined-basic" label="Red token per row" variant="outlined" />
-    </Box>
+const TokenInputs = (props) => {
+    return (
+        <>
+            <Box>
+                <form className='container'>
+                    <Box sx={{ display: "flex", flexDirection: 'column', justifyContent: 'center', marginRight: '30%', color: 'blue' }}>
+                        <Box className='blue-container'>Generate Blue Token</Box>
+                        <TextField label="No. of blue Tokens" variant="outlined" onChange={(e) => { props.setBlueToken(e.target.value) }} />
+                        <br />
+                        <TextField label="Blue token prefix" variant="outlined" onChange={(e) => { props.setBluePrefix(e.target.value) }} />
+                        <br />
+                        <TextField label="Blue token per row" variant="outlined" onChange={(e) => { props.setBlueRow(e.target.value) }} />
+                    </Box>
 
-    
-    </form>
-    <Box sx={{display:'flex',justifyContent:'center',marginTop:'50px'}}>
-        <Button  variant="contained">Generate</Button>
-        <Box sx={{paddingLeft:'50px'}}>
-        <Button variant="contained">Clear</Button>
-        </Box>
-    </Box>
-    </Box>
-    </>
-  )
+                    <Box sx={{ display: "flex", flexDirection: 'column', justifyContent: 'center', width: '30%' }}>
+                        <Box className='red-container'>Generate Red Token</Box>
+                        <TextField label="No. of Red Tokens" variant="outlined" />
+                        <br />
+                        <TextField label="Red token prefix" variant="outlined" />
+                        <br />
+                        <TextField label="Red token per row" variant="outlined" />
+                    </Box>
+
+                </form>
+                <Box sx={{ display: 'flex', justifyContent: 'center', marginTop: '50px' }}>
+                    <Button variant="contained" onClick={props.HandleGenerate}>Generate</Button>
+                    <Box sx={{ paddingLeft: '50px' }}>
+                        <Button variant="contained" onClick={props.clear}>Clear</Button>
+                    </Box>
+                </Box>
+            </Box>
+        </>
+    )
 }
 
 export default TokenInputs
